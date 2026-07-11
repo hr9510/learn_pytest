74daa83195c043861246911cea15fa099f4b1f57
hr9510 <himanshurajoriya9510@gmail.com>
2026-07-11 14:28:53 +0530

    Update environment variable for database URI

--- app/configuration/configuration.py
+++ app/configuration/configuration.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Testing_Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     Testing = True
